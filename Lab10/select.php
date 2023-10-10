fae05d80475820d722545eef623a359c0200ff10
6404062630465 <139141544+6404062630465@users.noreply.github.com>
2023-10-10 14:48:09 +0700

    Add files via upload

---
+++ Lab10/select.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+    <?php
+    if (isset($_GET["language"])) {
+        if (empty($_COOKIE["lang"])) {
+            setcookie("lang", $_GET["language"], time() + 10);
+        }
+        if (isset($_COOKIE["lang"])) {
+            setcookie("lang", $_GET["language"], time() + 10);
+        }
+    }
+    ?>
+    <a href="main.php">Go to main</a>
+</body>
+
+</html>
